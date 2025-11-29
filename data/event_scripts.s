325a2df714a0c80a945e23fd047ad520a3061e2d
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-11-29 23:29:01 +0100

    First map test

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1158,3 +1158,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/Onett/scripts.inc"
